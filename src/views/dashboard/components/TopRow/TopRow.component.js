ba5aee2d5418b2ca64a7a25b5a8221c67fd05a8f
Khushi Sharma <khushielectrovese@gmail.com>
2024-04-01 20:57:11 +0530

    top-row changes

--- src/views/dashboard/components/TopRow/TopRow.component.js
+++ src/views/dashboard/components/TopRow/TopRow.component.js
@@ -10,8 +10,12 @@ import {
 } from "@material-ui/core";
 
 import styles from "./Style.module.css";
+import data from './data.json';
+import trophy from '../../../../assets/Assets/trophy.png'
 
 const TopRow = () => {
+  
+  const { Data } = data;
 //   const dispatch = useDispatch();
 //   const { dashboard } = useSelector((state) => state?.dashboard);
 
@@ -21,25 +25,34 @@ const TopRow = () => {
 
   return (
     <>
-        <div className={styles.div1}>
+        <div className={styles.div1}> 
+        {Data.map((item, index) => ( 
           <div className={styles.congratulation} id={styles.card}>
-            <span
-              style={{  fontWeight: "700", fontSize: "22px" }}
+            
+            <div className="title"
+              style={{  fontWeight: "700", fontSize: "22px"}}
             >
-              Congratulations Shivay!
-            </span>
-            <span style={{  fontSize: "14px" }}>
-              Best Seller of the Month
-            </span>
-            <span style={{ color: "#25CEAE", fontSize: "24px" }}>$42.8k</span>
-            <span style={{  fontSize: "14px" }}>
-              {" "}
-              78% of target 🚀
-            </span>
-            <div>
-              <ButtonBase className={styles.login}>View Sales</ButtonBase>
+              {item.title}
+              </div>
+            <div className={styles.subtitle} style={{  fontSize: "14px", marginTop: "-28px"  }}>
+            {item.subtitle}
+            </div> 
+           
+            <div style={{ color: "#25CEAE", fontSize: "24px", fontWeight: "bold", marginTop: "-18px" }}>{item.amount}</div>
+            <div className={styles.subtitle} style={{  fontSize: "14px", marginTop: "-30px"  }}>
+             
+              {item.progress}
             </div>
+            <div className={styles.buttonAndImageContainer} >
+              <ButtonBase className={styles.login} >{item.buttonText}</ButtonBase>
+              {/* <img src={trophy} className={styles.image} /> */}
           </div>
+          
+          {/* <div>
+            <img src={trophy} className={styles.image} /></div> */}
+            </div>
+            
+           ))}
           <div className={styles.sales} id={styles.card}>
             <span
               style={{  fontWeight: "700", fontSize: "18px" }}
