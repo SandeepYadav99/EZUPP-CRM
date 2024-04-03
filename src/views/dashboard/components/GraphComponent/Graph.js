8f770f6dfd615ee92de7ea8de0f2baad7266122b
Deepesh Giri <deepeshelectrovese@gmail.com>
2024-04-03 09:22:51 +0530

    Initial Data Added

--- src/views/dashboard/components/GraphComponent/Graph.js
+++ src/views/dashboard/components/GraphComponent/Graph.js
@@ -10,8 +10,11 @@ import {
     ButtonBase,
   } from "@mui/material";
 import styles from "./Style.module.css";
+import { useState } from "react";
 
 const GraphComponent =()=>{
+  const [indexData,setIndexData] = useState(3)
+
     return (
         <div className={styles.paper}>
         <div className={styles.headerAlign}>
@@ -19,10 +22,10 @@ const GraphComponent =()=>{
             <Typography variant={"subtitle1"}> Quotes</Typography>
           </div>
           <div className={styles.alignDataValue}>
-            <Typography variant={"subtitle2"}> Today</Typography>
-            <Typography variant={"subtitle2"}> Week </Typography>
-            <Typography variant={"subtitle2"}> Month</Typography>
-            <Typography variant={"subtitle2"} className={styles.underLine}> Year</Typography>
+            <Typography variant={"subtitle2"} onClick={()=>setIndexData(0)} className={`${indexData===0 &&  styles.underLine}`}> Today</Typography>
+            <Typography variant={"subtitle2"} onClick={()=>setIndexData(1)} className={`${indexData===1 && styles.underLine}`}> Week </Typography>
+            <Typography variant={"subtitle2"} onClick={()=>setIndexData(2)} className={`${indexData===2 && styles.underLine}`}> Month</Typography>
+            <Typography variant={"subtitle2"} onClick={()=>setIndexData(3)} className={`${indexData===3 && styles.underLine}`}> Year</Typography>
           </div>
         </div>
         <DoughnutChart />
