8f6714bd0128a5e464656521755555f3978fd34d
Khushi Sharma <khushielectrovese@gmail.com>
2024-04-20 00:22:27 +0530

    unit view changes

--- src/views/Units/Create/UnitCreate.view.js
+++ src/views/Units/Create/UnitCreate.view.js
@@ -4,11 +4,11 @@ import styles from "./Style.module.css";
 import CustomTextField from "./../../../components/FormFields/TextField/TextField.component";
 import useEventFormHook from "./UnitCreate.hook";
 import CustomSelectField from "./../../../components/FormFields/SelectField/SelectField.component";
-import { Autocomplete, MenuItem, TextField, Tooltip } from "@mui/material";
-import CustomDatePicker from "./../../../components/FormFields/DatePicker/CustomDatePicker";
+import {  Tooltip, Typography } from "@mui/material";
+
 import CustomSwitch from "./../../../components/FormFields/CustomSwitch";
 import CustomCheckbox from "./../../../components/FormFields/CustomCheckbox";
-import {Delete as DeleteIcon, Info as InfoIcon} from "@mui/icons-material";
+import { Delete as DeleteIcon, Info as InfoIcon } from "@mui/icons-material";
 import {
   OutlineButton,
   PrimaryButton,
@@ -33,13 +33,14 @@ const EventForm = ({ isOpen, handleToggle, candidateId, isInterview }) => {
     <div>
       <div className={styles.resetPasswordWrapper}>
         <div className={styles.fieldWrapper}>
-        <h4 className={styles.infoTitle}>
-                        <div className={styles.heading}>Type</div>
-                        <Tooltip title="Info" aria-label="info" placement="right">
-                            <InfoIcon fontSize={'small'}/>
-                        </Tooltip>
-
-        </h4>
+          <div className={styles.infoTitle}>
+            <div className={styles.heading}>
+            <Typography variant="subtitle1">Type</Typography> 
+            </div>
+            <Tooltip title="Info" aria-label="info" placement="right">
+              <InfoIcon fontSize={"small"} />
+            </Tooltip>
+          </div>
           <CustomTextField
             isError={errorData?.title}
             errorText={errorData?.title}
@@ -52,30 +53,29 @@ const EventForm = ({ isOpen, handleToggle, candidateId, isInterview }) => {
               onBlurHandler("title");
             }}
           />
-          <div className={"formFlex"}>
-          <div className={"formGroup"}>
-            <CustomCheckbox
-              value={form?.userManage}
-              handleChange={() => {
-                changeTextData(!form?.userManage, "userManage");
-              }}
-              label={`Is General`}
-            />
-          
+          <div className={"formFlex"} style={{marginLeft: "-10px"}}>
+            <div className={"formGroup"}>
+              <CustomCheckbox
+                value={form?.userManage}
+                handleChange={() => {
+                  changeTextData(!form?.userManage, "userManage");
+                }}
+                label={`Is General`}
+              />
+            </div>
+            <div className={"formGroup"}>
+              <CustomSwitch
+                value={form?.is_all_day}
+                handleChange={() => {
+                  changeTextData(!form?.is_all_day, "is_all_day");
+                }}
+                label={`Active ?`}
+              />
+            </div>
+          </div>
         </div>
-        <div className={"formGroup"}>
-          <CustomSwitch
-            value={form?.is_all_day}
-            handleChange={() => {
-              changeTextData(!form?.is_all_day, "is_all_day");
-            }}
-            label={`Active ?`}
-          /></div>
-         </div>   
-        </div>
-        <div style={{float: 'right'}}>
+        <div style={{ float: "right" }}>
           <PrimaryButton onClick={handleSubmit}>SUBMIT</PrimaryButton>
-        
         </div>
       </div>
     </div>
