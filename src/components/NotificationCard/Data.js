aefbcd5829eeb54f371096884d6e01b21f84ed64
Khushi Sharma <khushielectrovese@gmail.com>
2024-04-29 10:30:24 +0530

    icons updated

--- src/components/NotificationCard/Data.js
+++ src/components/NotificationCard/Data.js
@@ -1,17 +1,17 @@
-import open from '../../assets/Assets/ic_opened.png';
-import profit from '../../assets/Assets/ic_customer.png';
+import payment from '../../assets/Assets/ic_notification_payment.png';
+import message from '../../assets/Assets/ic_notification_message.png';
 const data={
   "Data":[
     {
         "title": "Your Payment is pending",
         "body": "Attention: Your recent payment for product is still pending. Please settle the outstanding amount promptly to avoid any service disruptions.",
-        "image": profit,
+        "image": payment,
         "dateTime": "4:05 PM"   
     },
     {
         "title": "New Request Received",
         "body": "Description will be shown here",
-        "image": open,
+        "image": message,
         "dateTime": "2 days ago"   
     }
    
