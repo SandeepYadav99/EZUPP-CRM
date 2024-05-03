c9e79ce2cc06aabc2dc01e7f6c4862aea7abb2c9
charanjeet <charanjeet@electrovese.com>
2024-05-03 11:46:53 +0530

    checkbox and radio fields

--- src/components/RadioButtons/RadioButtonWithText.js
+++ src/components/RadioButtons/RadioButtonWithText.js
@@ -1,86 +1,42 @@
-import React, { useState } from "react";
-import { FormControlLabel, Radio, Checkbox, Typography } from "@mui/material";
-import Box from "@mui/material/Box";
+import React from "react";
+import {Checkbox, FormControlLabel, Radio} from "@mui/material";
 import styles from "./Style.module.css";
-import {useTheme} from "@mui/styles";
-import csx from 'classnames';
+import RadioTextWrapper from "./RadioTextWrapper";
+
 const RadioButtonWithText = ({
   title,
   description,
   checked,
   handleChange,
   value,
-  cardStyle,
-  inputType
+  cardStyle
 }) => {
-  const theme = useTheme();
-  //const CheckboxOrRadio = type === "checkbox" ? Checkbox : Radio;
   return (
-    <>
-      <Box
-        border={1}
-        sx={{
-          display: 'flex',
-          flexDirection:"column",
-          alignItems: "flex-start",
-          borderRadius: theme.spacing(1),
-          padding: theme.spacing(1),
-          borderColor: checked ? `${theme.palette.primary.main}` : "#D8D8DD",
-          cursor: 'pointer',
-      }}
-        borderWidth={2}
-        className={csx(styles.textcard, cardStyle ? cardStyle : {})}
-        onClick={() => {
-          handleChange && handleChange(value)
-      }}
-      >
-
-            <div className={styles.row}>
-            {inputType === 'radio' && (
-            <FormControlLabel
+      <RadioTextWrapper
+          description={description}
+          cardStyle={cardStyle}
+          handleChange={handleChange}
+          title={title}
+          checked={checked}
+          value={value}
+          fieldComp={<FormControlLabel
               control={
-                <Radio
-                  checked={checked}
-                  onChange={() => {
-                    // handleChange && handleChange(value)
-                }}
-                  //onChange={handleChange}
-                  value={value}
-                  size="small"
-                />
+                  <Radio
+                      checked={checked}
+                      onChange={() => {
+                          // handleChange && handleChange(value)
+                      }}
+                      //onChange={handleChange}
+                      value={value}
+                      size="small"
+                  />
               }
               className={styles.radio1}
-            />
-          )}
-          {inputType === 'checkbox' && (
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={checked}
-                onChange={() => {
-                  // handleChange && handleChange(value)
-              }}
-                value={value}
-                size="small"
-              />
-            }
-            className={styles.radio1}
-          />
-        )}
-            <div className={styles.textContent}>
-            <Typography variant="subtitle1" className="title">
-              {title}
-            </Typography>
-         
-          <Typography variant="body1" className="title">
-            {description}
-          </Typography>
-          </div>
-          </div>
-        
-      </Box>
-    </>
+          />}
+      />
   );
 };
 
 export default RadioButtonWithText;
+
+
