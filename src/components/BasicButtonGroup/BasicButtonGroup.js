3e7dc252f352f78fc35326593e0569009ca78312
charanjeet <charanjeet@electrovese.com>
2024-05-13 15:56:46 +0530

    basic button selected index will be an index

--- src/components/BasicButtonGroup/BasicButtonGroup.js
+++ src/components/BasicButtonGroup/BasicButtonGroup.js
@@ -4,7 +4,7 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import {useTheme} from "@mui/styles";
 import styles from "./Styles.module.css"
 import { Typography } from '@mui/material';
-export default function BasicButtonGroup({ buttonText }) {
+export default function BasicButtonGroup({ buttonText, selectedIndex }) {
     const [selectedButton, setSelectedButton] = useState(null);
     const handleButtonClick = (index) => {
         setSelectedButton(index);
@@ -18,13 +18,13 @@ export default function BasicButtonGroup({ buttonText }) {
           onClick={() => handleButtonClick(index)}
           className={`${styles.button} `}
           style={{
-            color: selectedButton === index ? 'white' : `#888888`, 
-             borderColor: '#E4E4E6', 
+            color: selectedButton === index ? 'white' : `#888888`,
+             borderColor: '#E4E4E6',
              backgroundColor: selectedButton === index ? `${theme.palette.primary.main}`: '#F5F5F5'
           }}
           variant={selectedButton === index ? "contained" : "outlined"}
         >
-        <Typography variant="body1">{text}</Typography> 
+        <Typography variant="body1">{text}</Typography>
         </Button>
       ))}
     </ButtonGroup>
