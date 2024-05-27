d1180667f0685e8a0eefe424f48322760a317f83
himanshu-199 <karn07rawat@gmail.com>
2024-05-27 16:06:33 +0530

    api error fields updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -90,6 +90,15 @@ export default {
         COMPANY_DRIVER_ADD: 'COMPANY_DRIVER_ADD',
         COMPANY_DRIVER_REMOVE: 'COMPANY_DRIVER_REMOVE',
     },
+    API_ERROR_OBJ: {
+        404: "API error",
+        500: "Server Error",
+        502: "Bad Gateway",
+        503: "Service Unavailable",
+        400: "Bad Request",
+        401: "Unauthorized",
+        403: "Forbidden",
+      },
     USER_TYPES: {
         CUSTOMER: 'CUSTOMER',
         MANUFACTURE: 'MANUFACTURER',
