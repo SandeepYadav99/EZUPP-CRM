6ec58f0ec98c694131c95a59780b817f38356bbb
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-06-04 17:06:22 +0530

    [REFACTOR] Add custom Component for Please press enter to add a category if not found in the search results.

--- src/components/FormFields/AutoCompleteText/MultiComplete.js
+++ src/components/FormFields/AutoCompleteText/MultiComplete.js
@@ -7,7 +7,7 @@ import { styled } from "@mui/material/styles";
 import { InputAdornment, InputBase } from "@mui/material";
 import { IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { useTheme } from "@mui/material/styles";
+import { useTheme } from "@mui/styles";
 
 const StyledChip = styled(Chip)(({ theme }) => ({
   paddingLeft: theme.spacing(0),
@@ -78,6 +78,9 @@ const CustomMultiComplete = ({
               InputLabelProps={{
                 sx: {
                   color: theme.palette.text.primary,
+                  "& .MuiInputLabel-root": {
+                    color: theme.palette.text.primary,
+                  },
                 },
               }}
               {...params}
@@ -135,6 +138,14 @@ const CustomMultiComplete = ({
             value?.map((option, index) => (
               <StyledChip
                 {...getTagProps({ index })}
+                sx={{
+                  color: theme.palette.text.primary,
+                  backgroundColor: theme.palette.contentDark,
+                  "& .MuiChip-deleteIcon": {
+                    color: theme.palette.text.primary,
+                  },
+                  
+                }}
                 avatar={
                   showImage ? <Avatar src={option?.image} alt={"Image"} /> : ""
                 }
