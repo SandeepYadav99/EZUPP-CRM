cd9f49d465b044c7c38a928be7c691eb4fd970c2
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-06-21 11:17:33 +0530

    [FEATURE] Custom ios Switch

--- src/components/FormFields/CustomIosSwitch.js
+++ src/components/FormFields/CustomIosSwitch.js
@@ -1,65 +1,87 @@
-import { Switch, FormControlLabel } from "@mui/material";
+
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 import { useTheme } from "@mui/styles";
-import React from "react";
-import styled from "styled-components";
 
-const CustomIosSwitch = ({ handleChange, label, value, ...rest }) => {
-  const theme = useTheme();
-
-  const BaseSwitch = styled(Switch)({
-    width: 49,
-    height: 22,
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 49,
+  height: 22,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
     padding: 0,
-    display: "flex",
-    marginLeft: theme.spacing(1.4),
-  
-    "& .MuiSwitch-switchBase": {
-      padding: 2,
-    
-      transition: "all 300ms !important",
-      "&.Mui-checked": {
-        transform: "translateX(26px)",
-
-        "& + .MuiSwitch-track": {
-          opacity: 1,
-          backgroundColor: theme.palette.switchTheme,
-        },
-        "&.Mui-disabled + .MuiSwitch-track": {
-          opacity: 0.5,
-        },
+    margin: 2,
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(26px)",
+      color: theme.palette.text.primary,
+      "& + .MuiSwitch-track": {
+        backgroundColor: theme.palette.switchTheme,
+        opacity: 1,
+        border: 0,
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
       },
     },
-    "& .MuiSwitch-thumb": {
-      width: 18,
-      height: 18,
-      borderRadius: "25px",
-      backgroundColor: theme.palette.contact,
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      // color: "#33cf4d",
+      border: "6px solid #fff",
     },
-    "& .MuiSwitch-track": {
-      opacity: 1,
-      borderRadius: "25px",
-      //  transition: ' 0.3ms ease-out',
-      // border: `1px solid ${theme.palette.text.primary}`,
-      backgroundColor: theme.palette.swithcTrack,
-      boxSizing: "border-box",
-      transition: theme.transitions.create(["background-color"], {
-        duration: 500,
-      }),
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      // color:
+      //   theme.palette.mode === "light"
+      //     ? theme.palette.grey[100]
+      //     : theme.palette.grey[600],
     },
-  });
-  return (
-    <FormControlLabel
-      control={
-        <BaseSwitch
-          sx={{ m: 1 }}
-          checked={value ? true : false}
-          onChange={(e) => handleChange(e.target.checked)}
-        />
-      }
-      label={label}
-      {...rest}
-    />
-  );
-};
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 18,
+    height: 18,
+    borderRadius: "25px",
+    backgroundColor: theme.palette.contact,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: "25px",
+    backgroundColor: theme.palette.swithcTrack,
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+  },
+}));
 
-export default CustomIosSwitch;
+export default function CustomIosSwitch({
+  handleChange,
+  label,
+  value,
+  ...rest
+}) {
+  const theme = useTheme();
+  return (
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <IOSSwitch
+            sx={{ m: 1 }}
+            checked={value ? true : false}
+            onChange={(e) => handleChange(e.target.checked)}
+            theme={theme}
+            defaultChecked
+          />
+        }
+        label={label}
+        {...rest}
+      />
+    
+    </FormGroup>
+  );
+}
